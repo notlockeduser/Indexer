7b58e36c3b4630d4c8708207f1b37f2fe89332b8
Bogdan <58120983+notlockeduser@users.noreply.github.com>
2021-05-17 19:38:08 +0300

    Add data
    Add data and initialize it

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class Main {
+
+    public static void main(String[] args) {
+        int V = 5;
+        int[] startIndex = new int[5];
+        int[] endIndex = new int[5];
+        int[] N = {12500, 12500, 12500, 12500, 50000};
+        File[] directions = {
+                new File("aclImdb//test//neg"),
+                new File("aclImdb//test//pos"),
+                new File("aclImdb//train//neg"),
+                new File("aclImdb//train//pos"),
+                new File("aclImdb//train//unsup")
+        };
+    }
 }
